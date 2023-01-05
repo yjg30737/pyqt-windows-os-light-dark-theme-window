80e1e76bc08a3165d2bfd4ba510e795e67ebdc56
Jung Gyu Yoon <yjg30737@gmail.com>
2023-01-05 17:25:44 +0900

    Bump ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-windows-os-light-dark-theme-window',
-    version='0.0.1',
+    version='0.0.11',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
